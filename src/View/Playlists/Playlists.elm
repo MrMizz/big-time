3ec33fa272ef756af9f154c07f8589538b7cffa5
Alexander Binaei <alex.binaei@carpe.io>
2021-07-19 18:57:57 -0500

    grid playlists

--- src/View/Playlists/Playlists.elm
+++ src/View/Playlists/Playlists.elm
@@ -1,7 +1,7 @@
 module View.Playlists.Playlists exposing (view)
 
 import Html exposing (Html)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, height, src, style)
 import Msg.Msg exposing (Msg)
 import View.Hero
 
@@ -14,15 +14,45 @@ view =
 body : Html Msg
 body =
     let
+        embed : String -> Html Msg
+        embed url =
+            Html.div
+                [ class "column is-6"
+                ]
+                [ Html.iframe
+                    [ src url
+                    , style "width" "100%"
+                    , height 380
+                    , style "frameBorder" "0"
+                    , style "allowtransparency" "true"
+                    , style "allow" "encrypted-media"
+                    ]
+                    []
+                ]
+
+        one : Html Msg
+        one =
+            embed "https://open.spotify.com/embed/playlist/754yiFuN0kJCIfPAvbOnMU?theme=0"
+
+        two : Html Msg
+        two =
+            embed "https://open.spotify.com/embed/playlist/38OnMQrMkmJbkUS9Z8xdJq"
+
+        three : Html Msg
+        three =
+            embed "https://open.spotify.com/embed/playlist/006vvJAdRvQsHq0PHQSGHK"
+
         html =
             Html.div
-                [ class "has-text-centered"
+                [ class "columns is-multiline"
                 ]
-                [ Html.text "Playlists"
+                [ one
+                , two
+                , three
                 ]
     in
     Html.div
-        [ class "container"
+        [ class "container has-text-centered"
         ]
         [ html
         ]
